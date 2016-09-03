ba765be66c1a21924d8317e9ba054716090c78c0
Roger Zander <roger@zander.ch>
2016-09-03 20:08:25 +0200

    Updated Script

--- Artifacts/windows-ConfigurationManagerAdmin/SetAdmin.ps1
+++ Artifacts/windows-ConfigurationManagerAdmin/SetAdmin.ps1
@@ -1,6 +1,6 @@
 #Add Domain Admins as Full Admins
 import-module ("C:\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1")  
-new-psdrive -Name $SiteCode -PSProvider "AdminUI.PS.Provider\CMSite" -Root "localhost"
+new-psdrive -Name "T01" -PSProvider "AdminUI.PS.Provider\CMSite" -Root "localhost"
 cd ((Get-PSDrive -PSProvider CMSite).Name + ':')
     
 New-CMAdministrativeUser -Name "$($env:userdomain)\domain admins" -RoleName "Full Administrator"
