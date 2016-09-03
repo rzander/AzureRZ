93fc98951190668a2a20a9aed9d78c3009badf55
Roger Zander <roger@zander.ch>
2016-09-03 17:43:14 +0200

    Fix to set permission in SCCM for Domain Admins

--- Artifacts/windows-ConfigurationManagerCurrentBranch(EVAL)/setupCMCB.ps1
+++ Artifacts/windows-ConfigurationManagerCurrentBranch(EVAL)/setupCMCB.ps1
@@ -109,9 +109,13 @@ Install-ADDSForest -CreateDnsDelegation:$false `
     #Add Domain Admins as Full Admins
     #Import SCCM PowerShell Module
     import-module (Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1)  
+    new-psdrive -Name $SiteCode -PSProvider "AdminUI.PS.Provider\CMSite" -Root "localhost"
     cd ((Get-PSDrive -PSProvider CMSite).Name + ':')
+    
     New-CMAdministrativeUser -Name "$($env:userdomain)\domain admins" -RoleName "Full Administrator"
 
+
+    #Add Tools
     & ".\ConfigMgrTools_setup.exe"
     & ".\CollectionCommander_setup.exe"
     & ".\SCCMCliCtr_setup.exe"
